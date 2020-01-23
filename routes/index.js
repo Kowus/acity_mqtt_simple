9888ecd03c84d38f0c96d65f47dc5977d1f4b97f
Kowus <barnabas.nomo@acity.edu.gh>
2020-01-23 11:46:05 +0000

    attach server sent events, post messages through stream

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,39 @@
-const express = require("express"),
+const express = require('express'),
   router = express.Router(),
-  { connect } = require("mqtt"),
-  { mqtt } = require("../config/env"),
-  client = connect(mqtt.url);
+  { connect } = require('mqtt'),
+  { mqtt } = require('../config/env'),
+  client = connect(mqtt.url),
+  debug = require('debug')('start-mqtt-acity:index');
 
 let i = 10000;
 
-client.subscribe("Temps", (err, granted) => {
+client.subscribe('Temps', (err, granted) => {
   if (err) return console.error(err);
-  console.log(`subscribed to ${granted[0].topic}`);
+  debug(`subscribed to ${granted[0].topic}`);
 });
 
-router.get("./stream", function(req, res, next) {
+router.get('/stream', function(req, res, next) {
   // set timeout as high as possible
   req.socket.setTimeout((i *= 6));
 
   res.writeHead(200, {
-    "Content-Type": "text/event-stream",
-    "Cache-Control": "no-cache",
-    Connection: "keep-alive"
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    Connection: 'keep-alive'
   });
 
-  res.write("\n");
+  res.write('\n');
 
   var timer = setInterval(function() {
-    res.write(":" + "\n");
+    res.write(':' + '\n');
   }, 2000);
 
-  client.on("message", function(topc, msg, pkt) {
-    res.write("data:" + msg + "\n\n");
+  client.on('message', function(topic, msg, pkt) {
+    debug(`Received ${msg} from ${topic}`);
+    res.write('data:' + msg + '\n\n');
   });
 
-  req.on("close", function() {
+  req.on('close', function() {
     clearTimeout(timer);
   });
 });
@@ -39,11 +41,11 @@ router.get("./stream", function(req, res, next) {
 // sub...
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-client.on("error", err => {
-  console.error(err);
+client.on('error', err => {
+  debug(err);
 });
 module.exports = router;

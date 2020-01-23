1ecc73017f7115b6dbafc3d2d7d8a795723c45bd
harrietfiagbor <harrietfiagbor@gmail.com>
2020-01-23 02:02:30 +0000

    setup stream and mqtt client

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express"),
+  router = express.Router(),
+  { connect } = require("mqtt"),
+  { mqtt } = require("../config/env"),
+  client = connect(mqtt.url);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+let i = 10000;
+
+client.subscribe("Temps", (err, granted) => {
+  if (err) return console.error(err);
+  console.log(`subscribed to ${granted[0].topic}`);
 });
 
+router.get("./stream", function(req, res, next) {
+  // set timeout as high as possible
+  req.socket.setTimeout((i *= 6));
+
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive"
+  });
+
+  res.write("\n");
+
+  var timer = setInterval(function() {
+    res.write(":" + "\n");
+  }, 2000);
+
+  client.on("message", function(topc, msg, pkt) {
+    res.write("data:" + msg + "\n\n");
+  });
+
+  req.on("close", function() {
+    clearTimeout(timer);
+  });
+});
+
+// sub...
+
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+client.on("error", err => {
+  console.error(err);
+});
 module.exports = router;
